5ac3a9d138211e4c686bd79facea6291923674f8
Jenny Q <jenny.q@me.com>
2019-08-26 17:02:45 -0400

    updating results list style/view

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,15 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp';
 import useBusinesses from '../hooks/useBusinesses'
+import ResultsList from '../components/ResultsList';
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
     const [searchApi, results, errorMessage] = useBusinesses();
-    
+
+    const filterResultsByPrice = (price) => {
+        return results.filter(result => {
+            return result.price === price;
+        })
+    }
     return (
-        <View>
+        <>
             <SearchBar
                 term= {term}
                 onTermChange={setTerm}
@@ -17,7 +23,12 @@ const SearchScreen = () => {
             <Text>Search Screen</Text>
             {errorMessage ? <Text>{errorMessage}</Text> : null}
             <Text>We have found {results.length} results</Text>
-        </View>
+            <ScrollView>
+                <ResultsList title="Cheap" results={filterResultsByPrice('$')}/>
+                <ResultsList title="Date Night" results={filterResultsByPrice('$$')} />
+                <ResultsList title="Special Events" results={filterResultsByPrice('$$$')} />
+            </ScrollView>
+        </>
     );
 };
 
